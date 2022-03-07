4dfab0ca58dce7b6f3b3f140faf642c9fe58a44f
shahbazkazmi110 <shahbaz@qubitse.com>
2022-03-07 17:28:37 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TracksController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\AlbumController;
+ 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +24,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/track', [TracksController::class, 'index']);
+// require __DIR__.'/auth.php';
+
+
+Route::get('/genre', [GenreController::class, 'index']);
+Route::get('/tag', [TagController::class, 'index']);
+Route::get('/artist', [ArtistController::class, 'index']);
+Route::get('/playlist', [PlaylistController::class, 'index']);
+Route::get('/language', [LanguageController::class, 'index']);
+Route::get('/album', [AlbumController::class, 'index']);
