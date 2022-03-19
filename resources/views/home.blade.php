69e55afe8d7924e9ae28ff89e006e457079bfd68
shahbazkazmi110 <shahbaz@qubitse.com>
2022-03-19 21:06:25 +0500

    image link issue solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -418,7 +418,7 @@
 		
 			<div class="col-xl-2 col-lg-3 col-md-4 col-6">
 				<div class="card card--playlist">
-					<div class="card--playlist__image" style="background-image: url('https://iLyrics.org/uploads/download_(32).jpg');">
+					<div class="card--playlist__image" style="background-image: url('{{ $artist->image_link }}');">
 						<div class="dropdown float-end">
 							<a href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
 								<img src="{{ asset('media/dote_dote_dote.svg')}}">
