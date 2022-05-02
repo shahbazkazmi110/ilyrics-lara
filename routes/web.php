e5590f9ab4cde7d35e2d2ba0f2cb70ad06b8ab3c
shahbazkazmi110 <shahbaz@qubitse.com>
2022-05-03 01:02:35 +0500

    login feature added

--- routes/web.php
+++ routes/web.php
@@ -81,4 +81,15 @@ Route::get('/privacy-policy', function() {
 });
 
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
 
