4748a0f4011772cd77d985b69e49f150edb42d47
Zainab <zainabhaider07@yahoo.com>
2022-05-09 03:58:34 +0500

    search controller : reciter, genre, tag queries finished

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,7 +13,18 @@ class SearchController extends Controller
     public function search_action(Request $request){
 
         $id = null;
-        $data["tracks"] = Track::getAllTracks();
+
+        $artist = "Aamir Baltistani";
+        $data["tracks_reciters"] = SearchController::getTracksByReciter($artist);
+
+        $genre = "salaam";
+        $data["tracks_genre"] = SearchController::getTracksByGenre($genre);
+
+        $tag = "azadari";
+        $data["tracks_tag"] = SearchController::getTracksByTag($tag);
+
+
+        $data["all_tracks"] = Track::getAllTracks();
 
         $data["tags"] = Tag::orderBy('title', 'ASC')->get();
         $data["genres"] = Genre::getGenre();
@@ -25,9 +36,10 @@ class SearchController extends Controller
         //     $data["track_list"] = SearchController::getTracks($request);
         // }
 
-        $data["track_list"] = Track::selectRaw('track.id, track.audio_type, track.title, artist.name as track_artists, track.view_count, track.resolution, track.contributor_id, 
-                    track.modified, track.album_year, track.track_duration as audio_duration, track.remote_duration, track.audio_link,
-                    artist.id AS artist_id, artist.name as artist_name, artist.image_name, track.track_name')
+        $data["tracks"] = Track::
+        select('track.id', 'track.audio_type', 'track.title','track.artists',  'track.view_count', 'track.resolution', 
+                'track.contributor_id', 'track.modified', 'track.album_year', 'track.track_name', 'track.track_duration as audio_duration',
+                'track.remote_duration', 'track.audio_link', 'artist.id AS artist_id', 'artist.name AS artist_name', 'artist.image_name',  'artist.resolution as artist_resolution')
         ->join('artist', DB::raw("FIND_IN_SET(artist.id,track.artists)"),'>',DB::raw("'0'"))
        ->where('track.status', 1)
        ->orderBy('track.title', 'ASC')
@@ -40,9 +52,10 @@ class SearchController extends Controller
 
 
 
-       return view('search.search', $data);
 
-       //return $data;
+       //return view('search.search', $data);
+
+       return $data;
 
     }
 
@@ -89,9 +102,61 @@ class SearchController extends Controller
     }
 
 // Search By Artist
-// Search By Reciters
+    public function getTracksByReciter($artist){
+        $tracks = Track::
+        selectRaw('track.id, track.audio_type, track.title, artist.name as track_artists, track.view_count, track.resolution, track.contributor_id, 
+                track.modified, track.album_year, track.track_duration as audio_duration, track.remote_duration, track.audio_link,
+                artist.id AS artist_id, artist.name as artist_name, artist.image_name, track.track_name')
+        ->join('artist', DB::raw("FIND_IN_SET(artist.id,track.artists)"),'>',DB::raw("'0'"))
+        ->where('track.status', 1)
+        ->where('artist.name', 'LIKE', '%' . $artist . '%')
+        ->orderBy('track.title', 'ASC')
+        ->get();
+
+        return $tracks;
+    }
 // Search By Genre
+    public function getTracksByGenre($genre)
+    {
+        $tracks = Track::
+        selectRaw('track.id, track.audio_type, track.title, artist.name as track_artists, track.view_count, track.resolution, track.contributor_id, 
+                track.modified, track.album_year, track.track_duration as audio_duration, track.remote_duration, track.audio_link,
+                artist.id AS artist_id, artist.name as artist_name, artist.image_name, track.track_name')
+        ->join('artist', DB::raw("FIND_IN_SET(artist.id, track.artists)"),'>',DB::raw("'0'"))
+        ->join('genre', DB::raw("FIND_IN_SET(genre.id, track.genres)"),'>',DB::raw("'0'"))
+        ->where('track.status', 1)
+        ->where('genre.title', 'LIKE', '%' . $genre . '%')
+        ->orderBy('track.title', 'ASC')
+        ->get();
+
+        return $tracks;
+    }
+
+// Search By Tag
+    public function getTracksByTag($tag)
+    {
+        $tracks = Track::
+        selectRaw('track.id, track.audio_type, track.title, artist.name as track_artists, track.view_count, track.resolution, track.contributor_id, 
+                track.modified, track.album_year, track.track_duration as audio_duration, track.remote_duration, track.audio_link,
+                artist.id AS artist_id, artist.name as artist_name, artist.image_name, track.track_name')
+        ->join('artist', DB::raw("FIND_IN_SET(artist.id, track.artists)"),'>',DB::raw("'0'"))
+        ->join('tag', DB::raw("FIND_IN_SET(tag.id, track.tags)"),'>',DB::raw("'0'"))
+        ->where('track.status', 1)
+        ->where('tag.title', 'LIKE', '%' . $tag . '%')
+        ->orderBy('track.title', 'ASC')
+        ->get();
+
+        return $tracks;
+    }
 
 
 
 }
+
+
+
+
+
+// Track::selectRaw('track.id, track.audio_type, track.title, artist.name as track_artists, track.view_count, track.resolution, track.contributor_id, 
+//                     track.modified, track.album_year, track.track_duration as audio_duration, track.remote_duration, track.audio_link,
+//                     artist.id AS artist_id, artist.name as artist_name, artist.image_name, track.track_name')
--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -13,7 +13,7 @@
             <input type="text" class="form-control form-control--large" placeholder="Search by Reciter..." aria-label="Recipient's username" aria-describedby="button-addon2">
             <input type="text" class="form-control form-control--large" placeholder="Search by Genre..." aria-label="Recipient's username" aria-describedby="button-addon2">
             <input type="text" class="form-control form-control--large" placeholder="Search by Tag..." aria-label="Recipient's username" aria-describedby="button-addon2">
-            <button class="btn btn--large" type="button" id="button-addon2" style="min-width:80px;"><img src="{{ asset('media/search_white.svg')}}"></button>
+            <button class="btn btn--large" type="button" id="button-addon2" style="min-width:200px;"><img src="{{ asset('media/search_white.svg')}}"></button>
           </div>
         </div>
 
@@ -26,55 +26,8 @@
     
 @section('content')
 <main>
-<div class="container pt-md-5 mb-5 pb-5">
-    <div class="row" id="pagination-data">
-
-        @foreach ($track_list as $track) 
-        <!-- player starts here -->
-        <div id="ag2" class="audiogallery skin-wave auto-init" style="opacity:0; margin-top:30px;"
-            data-options='{
-            "cueFirstMedia": "on",
-            "autoplay": "off",
-            "autoplayNext": "on",
-            "design_menu_position": "bottom",
-            "enable_easing": "on",
-            "playlistTransition": "fade",
-            "design_menu_height": "200"
-            }'
-            >
-            <!-- options for playlist in data-options -->
-            <div class="items">
-                <div class="audioplayer-tobe skin-wave button-aspect-noir" data-thumb="'{{ \App\Helpers\Helper::format_image($track->image_name) }}'"
-                    data-type="audio"
-                    data-source="{{ \App\Helpers\Helper::format_track($track->audio_type == 1 ? $track->track_name : $track->audio_link, $track->audio_type) }}"
-                    data-options='{
-                    "settings_php_handler": "inc/php/publisher.php",
-                    "skinwave_comments_enable": "on",
-                    "skinwave_comments_retrievefromajax": "on",
-                    "pcm_data_try_to_generate": "on",
-                    "pcm_data_try_to_generate_wait_for_real_pcm": "on",
-                    "skinwave_wave_mode_canvas_waves_number": 3,
-                    "skinwave_wave_mode_canvas_waves_padding": 1,
-                    "skinwave_wave_mode_canvas_reflection_size": 0.25,
-                    "design_color_bg": "444444",
-                    "design_color_highlight": "aa4444"
-                    }'
-                    >
-                    <!-- options for player in data-options -->
-                    <div class="feed-dzsap feed-artist"><a href="/artist/{{ $track->artist_id }}">{{$track->track_artists}}</a></div>
-                    <div class="feed-dzsap feed-songname"><a  href="{{ route('tracks-by-id', ['track_id' => $track->id]) }}" >{{$track->title}}</a></div>
-                </div>
-            </div>
-        </div>
-        <div class="border-bottom mb-2 pb-2 text-md-end text-center pt-2 pt-md-0">
-            <button class="btn btn-sharing" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Add Favorites</button>
-            <button class="btn btn-sharing" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Add to Playlist</button>
-            <button class="btn btn-sharing" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Download</button>
-            <button class="btn btn-sharing" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Share</button>
-        </div>
-        <!-- / player starts here -->
-    @endforeach   
-    </div>
+  <div  class="container pt-md-5 mb-5 pb-5"> 
+    <x-load-tracks :tracks="$tracks"/>
     <div class="mt-2">
       <div class="ajax-load">
         <div class="loader spinner-border text-success" role="status">
