7d51e94c9f3c936a560e6d3901b80ff6ff6a95f1
shahbazkazmi110 <shahbaz@qubitse.com>
2022-06-14 00:08:26 +0500

    collection page fix

--- resources/views/my-collections.blade.php
+++ resources/views/my-collections.blade.php
@@ -18,67 +18,12 @@
     <div class="row mb-5 pb-5">
         @if($my_playlists->count())
         @foreach($my_playlists as $playlist)
-            <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                <a href="{{ route('tracks-by-playlist', ['id' => $playlist->id]) }}">				
-                    <div class="card card--playlist">
-                        <div class="card--playlist__image" style="background-image: url('{{ \App\Helpers\Helper::format_image($playlist->image_name,1) }}');">
-                            
-                            {{-- <div class="dropdown float-end">
-                                <a href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="{{ asset('media/dote_dote_dote.svg')}}">
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="dropdownMenuButton1">
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/file-earmark-plus.svg')}}"> Add to your Playlist</a></li>
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/collection-play.svg')}}"> Play All</a></li>
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/file-earmark-arrow-down.svg')}}"> Download</a></li>
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/share-fill.svg')}}"> Share</a></li>
-                                </ul>
-                            </div> --}}
-                        </div>
-                        <div class="card--playlist__content">
-                            {{$playlist->title}}
-                        </div>
-                        <div>
-                            @if(Auth::user())
-                                @php 
-                                    $saved =  \App\Helpers\Helper::isSavedPlaylist($playlist->id); 
-                                    $icon = $saved ? asset('media/bookmark-dark.svg') : asset('media/bookmark.svg');
-                                @endphp	
-                                <a href="#" class="toggle-save-playlist position-absolute" style="z-index:1;" data-saved="{{ $saved ? 'yes': 'no' }}" data-playlist-id="{{$playlist->id}}" >
-                                    <img src="{{ $icon }}" alt="Playlist Icon">
-                                </a>
-                            @endif
-                        </div>
-                        <div class="card--playlist__tracks">Total Tracks : {{$playlist->track_count}}</div>
-                    </div>
-                </a>
-            </div>
+            <x-playlist-card :playlist="$playlist"/>
         @endforeach
         @else
             <p>No Playlists Added</p>    
         @endif
-        {{-- <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <div class="card card--playlist">
-                <div class="card--playlist__image" style="background-image: url('https://iLyrics.org/uploads/download_(32).jpg');">
-                    <div class="dropdown float-end">
-                        <a href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="assets/media/dote_dote_dote.svg">
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/file-earmark-plus.svg"> Add to your Playlist</a></li>
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/collection-play.svg"> Play All</a></li>
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/file-earmark-arrow-down.svg"> Download</a></li>
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/share-fill.svg"> Share</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="card--playlist__content">
-                    Wiladat e Imam Ali a.s
-                </div>
-                <div class="card--playlist__tracks">21 Tracks</div>
-            </div>
-        </div> --}}
-     </div>
+    </div>
     <!-- My Playlist ended -->
     
     
@@ -96,70 +41,13 @@
     <div class="row mb-5 pb-5">
         @if($saved_playlists->count())
         @foreach($saved_playlists as $playlist)
-            <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-                <a href="{{ route('tracks-by-playlist', ['id' => $playlist->id]) }}">				
-                    <div class="card card--playlist">
-                        <div class="card--playlist__image" style="background-image: url('{{ \App\Helpers\Helper::format_image($playlist->image_name,1) }}');">
-                            
-                            {{-- <div class="dropdown float-end">
-                                <a href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="{{ asset('media/dote_dote_dote.svg')}}">
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="dropdownMenuButton1">
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/file-earmark-plus.svg')}}"> Add to your Playlist</a></li>
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/collection-play.svg')}}"> Play All</a></li>
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/file-earmark-arrow-down.svg')}}"> Download</a></li>
-                                    <li><a class="dropdown-item" href="#"><img class="mr-2" src="{{ asset('media/share-fill.svg')}}"> Share</a></li>
-                                </ul>
-                            </div> --}}
-                        </div>
-                        <div class="card--playlist__content">
-                            {{$playlist->title}}
-                        </div>
-                        <div>
-                            @if(Auth::user())
-                                @php 
-                                    $saved =  \App\Helpers\Helper::isSavedPlaylist($playlist->id); 
-                                    $icon = $saved ? asset('media/bookmark-dark.svg') : asset('media/bookmark.svg');
-                                @endphp	
-                                <a href="#" class="toggle-save-playlist position-absolute" style="z-index:1;" data-saved="{{ $saved ? 'yes': 'no' }}" data-playlist-id="{{$playlist->id}}" >
-                                    <img src="{{ $icon }}" alt="Playlist Icon">
-                                </a>
-                            @endif
-                        </div>
-                        <div class="card--playlist__tracks">Total Tracks : {{$playlist->track_count}}</div>
-                    </div>
-                </a>
-            </div>
+            <x-playlist-card :playlist="$playlist"/>
         @endforeach            
         @else
             <p>No Saved Playlists Available</p>
         @endif
-        {{-- <div class="col-xl-2 col-lg-3 col-md-4 col-6">
-            <div class="card card--playlist">
-                <div class="card--playlist__image" style="background-image: url('https://iLyrics.org/uploads/download_(32).jpg');">
-                    <div class="dropdown float-end">
-                        <a href="javascript:void(0);" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="assets/media/dote_dote_dote.svg">
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/file-earmark-plus.svg"> Add to your Playlist</a></li>
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/collection-play.svg"> Play All</a></li>
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/file-earmark-arrow-down.svg"> Download</a></li>
-                            <li><a class="dropdown-item" href="#"><img class="mr-2" src="assets/media/share-fill.svg"> Share</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="card--playlist__content">
-                    Wiladat e Imam Ali a.s
-                </div>
-                <div class="card--playlist__tracks">21 Tracks</div>
-            </div>
-        </div> --}}
     </div>
     <!-- Saved Playlist ended -->
-    
-    
     
     <!-- Favourite Tracks -->
     <div class="row pb-4">
